cacb2b6ebe37982e61e28ddc3de22476ad54408d
Kurt Guardia <kurtguardia@gmail.com>
2020-11-03 18:27:19 +0100

    inital folder structure setup & dependencies installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Home, Favorites, User, NotFound } from "./Views/Index";
+import Player from "./Components/Player/Player";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Sidebar />
+        <Player />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/favorites" component={Favorites} />
+          <Route path="/user" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+//1db954
+//1ed760
+//191414
---
+++ src/Views/Index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home/Home";
+export { default as Favorites } from "./Favorites/Favorites";
+export { default as User } from "./User/User";
+export { default as NotFound } from "./404/NotFound";
