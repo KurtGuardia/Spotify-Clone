f675fea4c4d13e5d636e09846c17641d63f6182a
Kurt Guardia <kurtguardia@gmail.com>
2020-11-05 18:05:58 +0100

    login form validation finished

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -5,9 +5,27 @@ import { ReactComponent as SpotifyIcon } from "../../assets/images/spotify.svg";
 const Login = () => {
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
+  const [isError, setError] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const checkError = () => {
+    if (user !== "testUser" && password !== "Test123") {
+      setError(true);
+      setMessage("Please enter 'testUser' for User and 'Test123' for Password");
+    } else if (user !== "testUser") {
+      setError(true);
+      setMessage("Please enter 'testUser' for User");
+    } else if (password !== "Test123") {
+      setError(true);
+      setMessage("Please enter 'Test123' for Password");
+    } else {
+      setError(false);
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ user, password });
+    checkError();
   };
 
   return (
@@ -43,6 +61,8 @@ const Login = () => {
         </div>
 
         <button className="login__btn">LOGIN</button>
+
+        {isError && <p className="login__error-msg">{message}</p>}
       </form>
     </div>
   );
