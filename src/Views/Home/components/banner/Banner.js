d20350bba8cee9babccd54a354d40777c4fa7537
Kurt Guardia <kurtguardia@gmail.com>
2020-11-10 19:52:26 +0100

    basic setup and implemented the banner (wrong component)

---
+++ src/Views/Home/components/banner/Banner.js
@@ -0,0 +1,43 @@
+import "./Banner.scss";
+import { ReactComponent as Band } from "../../../../assets/icons/Group.svg";
+
+const Banner = () => {
+  const year = new Date().getFullYear();
+  const day = new Date().getDate();
+  const month = new Date().getMonth();
+
+  const formatMonth = (month) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    return months[month];
+  };
+
+  const date = `${formatMonth(month)} the ${day} of ${year}`;
+
+  return (
+    <div className="banner">
+      <div className="banner__left">
+        <h2 className="banner__title">Welcome Kurt</h2>
+        <p className="banner__date">Today: {date}</p>
+        <p className="banner__msg">Play any playlists you want</p>
+      </div>
+      <div className="banner__right">
+        <Band />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
