2413155322cd2d16b6c3da8d356f33a31745d4af
Kurt Guardia <kurtguardia@gmail.com>
2020-11-11 20:53:01 +0100

    added playlists rendering with dummy data display on dashboard

--- src/Views/Home/components/playlists/Playlists.js
+++ src/Views/Home/components/playlists/Playlists.js
@@ -1,9 +1,40 @@
+import { useState } from "react";
 import "./Playlists.scss";
+import PlaylistItem from "./Playlist/Playlist";
+
+const playlistsData = [
+  {
+    id: "a",
+    title: "Heavy",
+    songs: ["Enter Sandman", "Psychosocial", "B.Y.O.B"],
+  },
+  {
+    id: "b",
+    title: "Clasicos",
+    songs: ["Mozart", "Beethoven", "Debussy"],
+  },
+  {
+    id: "c",
+    title: "Old Days",
+    songs: ["Frank", "Queen"],
+  },
+  {
+    id: "e",
+    title: "Nostalgia",
+    songs: ["El Dorado", "Planeta del Tesoro", "Hercules"],
+  },
+];
 
 const Playlists = () => {
+  const [playlists] = useState(playlistsData);
+
   return (
     <div className="playlists">
-      <h1>Playlists</h1>
+      <div className="playlists__container">
+        {playlists.map((playlist) => {
+          return <PlaylistItem key={playlist.id} {...playlist} />;
+        })}
+      </div>
     </div>
   );
 };
