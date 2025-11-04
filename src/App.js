b9a1827f4da247e7d2c09115ac8d34cbe10fb9b3
Kurt Guardia <kurtguardia@gmail.com>
2025-11-04 11:55:36 +0400

    hotfix(modal) Some weird bug on modalData import

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ import Player from "./Components/Player/Player";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import { useSelector } from "react-redux";
 import Modal from "./Components/UI/Modal/Modal";
-import { modalData } from "./config/modalData";
+
+const modalData =
+  "Spotify Clone, was a project developed by Kurt Guardia and Oliver Castro, it is a no profit project. Built with ReactJS, Redux, SASS and Firebase. This is a functional app, in which the user can navigate around different playlists, you can save your favorite songs by liking them clicking on the heart, and they will be displayed on the Favorites tab. Activate the loop functionality to repeat the playlist when it finishes, or the random functionality after each song. You may also create a fake account, to authenticate with that user and password. It is not a 100% functional, you may not be able to use the Search bar, as it doesn't have functionality, of use the sidebar to chose a playlist nor play the song while in the favorites tab, all this could have been done, but with all the functionalities that it has now shows the knowldge we are trying to show on ReactJs, Redux, SaaS, Firebase and responsive design";
+
 
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
